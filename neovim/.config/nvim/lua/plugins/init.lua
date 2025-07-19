6005a7424c3447108a9a7d2b288b1f7345f38583
thebluenowhere <thebluenowhere@protonmail.com>
2025-07-19 23:26:21 +0100

    added telescope nvim plugin

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -1,2 +1,11 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return {
+    -- Adding cattpuccin theme. 
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    -- Adding telescope plugin for fuzzy finding 
+    { 
+        'nvim-telescope/telescope.nvim', 
+        tag = '0.1.8', 
+        dependencies = { 'nvim-lua/plenary.nvim' } 
+    }
+}
 
