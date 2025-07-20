dc7df681f5aedb363ee2e517911c29a93c17dd81
thebluenowhere <thebluenowhere@protonmail.com>
2025-07-20 14:22:12 +0100

    added python formatters

--- neovim/.config/nvim/lua/plugins/none-ls.lua
+++ neovim/.config/nvim/lua/plugins/none-ls.lua
@@ -5,8 +5,11 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.rubocop, 
-        null_ls.builtins.formatting.rubocop,
+				null_ls.builtins.diagnostics.rubocop,
+				null_ls.builtins.formatting.rubocop,
+				null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.black,
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,12 @@
-return  {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate", 
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = {"lua", "javascript", "python", "ruby", "rust"}, 
-      highlight = { enabled = true }, 
-      indent = {enabled = true }, 
-    })
-  end
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_install = true,
+			highlight = { enabled = true },
+			indent = { enabled = true },
+		})
+	end,
 }
-
